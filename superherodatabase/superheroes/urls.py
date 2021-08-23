28a90835b8476e4457c1844d1764aea25ea1b9ce
David Doll <ddoll8@yahoo.com>
2021-08-22 22:01:28 -0500

    added a new view.html page to view all of the superheros from the database

--- superherodatabase/superheroes/urls.py
+++ superherodatabase/superheroes/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name='superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('view/', views.view, name='view'),
+    path('<int:superhero_id>/', views.detail, name='detail')
 ]
 
 
--- superherodatabase/superheroes/views.py
+++ superherodatabase/superheroes/views.py
@@ -1,11 +1,22 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Superhero
 
 # Create your views here.
 def index(request):
+  return render(request, 'superheroes/index.html')
+
+def view(request):
   all_superheroes = Superhero.objects.all()
   context = {
     'all_superheroes': all_superheroes
   }
-  return render(request, 'superheroes/index.html', context)
+  return render(request, 'superheroes/view.html', context)
+
+def detail(request, superhero_id):
+  try:
+    superhero = Superhero.objects.get(pk=superhero_id)
+  except Superhero.DoesNotExist:
+    raise Http404("Superhero does not exist")
+  return render(request, 'superheroes/detail.html')
